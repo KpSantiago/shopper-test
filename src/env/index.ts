65f86a931bc5dc15e70018c498234f438b510e5f
KpSantiago <santiago.kauajose11@gmail.com>
2024-08-27 16:53:25 -0300

    feat: add verification to environment variables

---
+++ src/env/index.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+import "dotenv/config";
+
+const envSchema = z.object({
+    NODE_ENV: z.enum(["dev", "production", "test"]).default("dev"),
+    PORT: z.coerce.number().default(3333),
+    DATABASE_URL: z.string(),
+    GEMINI_API_KEY: z.string()
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (_env.success == false) {
+    console.error("Unexpected error with environment variables at env file", _env.error.format());
+    throw new Error();
+}
+
+export const env = _env.data;
