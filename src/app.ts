f9a7a54d73f2d0d043ae61d66bdc6c43728e5619
KpSantiago <santiago.kauajose11@gmail.com>
2024-08-28 18:56:16 -0300

    feat: add image endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
 import fastify from "fastify";
+import fastifyStatic from "@fastify/static";
+import path from "path";
 
 const app = fastify();
+
+app.register(fastifyStatic, {
+    root: path.join(__dirname, './images/'),
+    prefix: '/images/',
+});
 
 export { app }
