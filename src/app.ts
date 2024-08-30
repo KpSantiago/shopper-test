b1ec79a4bb1b0e3f0fb36768c0651258fdcb114e
KpSantiago <santiago.kauajose11@gmail.com>
2024-08-30 08:16:58 -0300

    refactor: add error_description to validation errors

--- src/app.ts
+++ src/app.ts
@@ -15,21 +15,27 @@ app.setErrorHandler((error, _request, reply) => {
                 if (i.code == "invalid_enum_value") {
                     return {
                         error_code: "INVALID_TYPE",
-                        message: i.message,
-                        received: i.received,
-                        path: i.path,
-                        options: i.options,
+                        error_description: {
+                            message: i.message,
+                            received: i.received,
+                            path: i.path,
+                            options: i.options,
+                        }
                     };
                 }
 
                 return {
                     error_code: "INVALID_DATA",
-                    message: i.message,
-                    path: i.path,
+                    error_description: {
+                        message: i.message,
+                        path: i.path,
+                    }
                 };
             })
         });
     }
+
+    return reply.status(500).send({ message: "Internal server Error" })
 })
 
 app.register(fastifyStatic, {
